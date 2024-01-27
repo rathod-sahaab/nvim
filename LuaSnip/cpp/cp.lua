06e8df476793122d64a791de267aa7a812264097
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-01-27 17:19:21 +0530

    feat: cp.lua

---
+++ LuaSnip/cpp/cp.lua
@@ -0,0 +1,30 @@
+return {
+  s(
+    "comp",
+    fmt(
+      [[
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define cerr                                                                   \
+  if (false)                                                                   \
+  cerr
+#endif // DEBUG
+
+using ll = long long;
+using ull = unsigned long long;
+
+int main(int argc, char *argv[]) {{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+{}
+
+  return 0;
+}}
+]],
+      { i(0) }
+    )
+  ),
+}
